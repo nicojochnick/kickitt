a9a978c6aa0e3bae36bdc903bc2e127876328cf9
Nico Jochnick <nico.jochnick@berkeley.edu>
2019-07-18 17:28:39 -0700

    first commit"

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Fragment } from "react";
+import {
+  View,
+  StyleSheet,
+} from "react-native";
+
+import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <View> </View>
+    );
+  }
+}
+
+
